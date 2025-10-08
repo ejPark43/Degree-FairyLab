e63d1eb10f71f113a15b348f8016839966a6e76d
ejPark43 <pej22200310@handong.ac.kr>
2025-10-08 16:46:36 +0900

    add: peoplefirstNeatLoud 폰트 추가

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -12,8 +12,9 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-family: "Pretendard", "Courier New", Courier, monospace;
+  font-family: "PeoplefirstNeatLoud", "Courier New", Courier, monospace;
   font-weight: 100;
+  font-size: 20px;
   width: 100%;
   height: 50px;
   background-color: ${lightTheme.colors.primary};
